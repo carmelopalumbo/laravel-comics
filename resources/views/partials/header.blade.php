89afc74b56f0e9a6085c3182c720963496572b13
Carmelo Palumbo <carmelopalumbo@outlook.it>
2022-12-22 15:36:30 +0100

    add-header

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,1 +1,49 @@
-<h1>HEADER</h1>
+<div class="start-box">
+    <div class="text">
+        <p>DC POWER VISA</p>
+        <p>ADDITIONAL DC SITES</p>
+    </div>
+</div>
+
+<header>
+    <img src="{{ Vite::asset('resources/images/dc-logo.png') }}" alt="logo" />
+    <ul>
+        <li>
+            <a class="{{ Route::currentRouteName() === 'characters' ? 'active' : '' }}"
+                href="{{ route('characters') }}">CHARACTERS</a>
+        </li>
+        <li>
+            <a class="{{ Route::currentRouteName() === 'comics' ? 'active' : '' }}"
+                href="{{ route('comics') }}">COMICS</a>
+        </li>
+        <li>
+            <a class="{{ Route::currentRouteName() === 'movies' ? 'active' : '' }}"
+                href="{{ route('movies') }}">MOVIES</a>
+        </li>
+        <li>
+            <a class="{{ Route::currentRouteName() === 'tv' ? 'active' : '' }}" href="{{ route('tv') }}">TV</a>
+        </li>
+        <li>
+            <a class="{{ Route::currentRouteName() === 'games' ? 'active' : '' }}"
+                href="{{ route('games') }}">GAMES</a>
+        </li>
+        <li>
+            <a class="{{ Route::currentRouteName() === 'collectibles' ? 'active' : '' }}"
+                href="{{ route('collectibles') }}">COLLECTIBLES</a>
+        </li>
+        <li>
+            <a class="{{ Route::currentRouteName() === 'videos' ? 'active' : '' }}"
+                href="{{ route('videos') }}">VIDEOS</a>
+        </li>
+        <li>
+            <a class="{{ Route::currentRouteName() === 'fans' ? 'active' : '' }}" href="{{ route('fans') }}">FANS</a>
+        </li>
+        <li>
+            <a class="{{ Route::currentRouteName() === 'news' ? 'active' : '' }}" href="{{ route('news') }}">NEWS</a>
+        </li>
+        <li>
+            <a class="{{ Route::currentRouteName() === 'shop' ? 'active' : '' }}" href="{{ route('shop') }}">SHOP</a>
+        </li>
+    </ul>
+    <input type="search" name="Search" id="" placeholder="Search">
+</header>
