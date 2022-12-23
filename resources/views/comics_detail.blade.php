8ecf20584269ac8a276709131d70e781ee63a592
Carmelo Palumbo <carmelopalumbo@outlook.it>
2022-12-23 17:21:29 +0100

    add-details-style

--- resources/views/comics_detail.blade.php
+++ resources/views/comics_detail.blade.php
@@ -7,28 +7,32 @@
 
                 <div class="thumb-detail">
                     <img src="{{ $product['thumb'] }}" alt="{{ $product['title'] }}">
-                    <p>{{ $product['series'] }}</p>
+                    <p>{{ $product['type'] }}</p>
                     <p>VIEW GALLERY</p>
                 </div>
 
                 <div class="descr">
                     <h3>{{ $product['title'] }}</h3>
                     <div class="box-price">
-                        <p>{{ $product['price'] }}</p>
+                        <p>PRICE: {{ $product['price'] }}</p>
+                        <p>AVAILABLE</p>
                     </div>
-                    <p>{{ $product['description'] }}</p>
+                    <p class="text">{{ $product['description'] }}</p>
                 </div>
 
                 <div class="footer-descr">
                     <div class="talent">
-                        <h4>Art by: {{ implode(', ', $product['artists']) }} | Written by:
-                            {{ implode(', ', $product['writers']) }} </h4>
+                        <h4>Talent</h4>
+                        <p> <strong>Art by:</strong> {{ implode(', ', $product['artists']) }} </p>
+                        <p><strong>Written by:</strong>
+                            {{ implode(', ', $product['writers']) }}</p>
                     </div>
 
                     <div class="specs">
-                        <p>Series: {{ $product['series'] }}</p>
-                        <p>Series: {{ $product['price'] }}</p>
-                        <p>Series: {{ $product['sale_date'] }}</p>
+                        <h4>Specs</h4>
+                        <p> <strong>Series:</strong>{{ $product['series'] }}</p>
+                        <p><strong>Price:</strong> {{ $product['price'] }}</p>
+                        <p><strong>On Sale Date:</strong> {{ $product['sale_date'] }}</p>
                     </div>
                 </div>
             </div>
